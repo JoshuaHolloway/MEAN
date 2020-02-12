7d22145e6f7463305aa050c99ba91b4ae9abf346
JoshuaHolloway <jhollow6@asu.edu>
2020-02-12 17:34:59 -0600

    Added express

--- 03_Node/302/server.js
+++ 03_Node/302/server.js
@@ -1,52 +1,14 @@
-const app = require("./backend/app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+console.log('server.js');
 
-const normalizePort = val => {
-    var port = parseInt(val, 10);
+const http = require('http');
+const app = require('./backend/app');
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-};
-
-const onError = error => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
-
-const onListening = () => {
-    const addr = server.address();
-    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-    debug("Listening on " + bind);
-};
-
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
+// Configure port-number
+const dev_port_num = 3e3;
+const default_host_port = process.env.PORT;
+const port = dev_port_num || default_host_port;
+app.set('port', port);
 const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+
+
+server.listen(port, () => { console.log('http://localhost:3000'); });
