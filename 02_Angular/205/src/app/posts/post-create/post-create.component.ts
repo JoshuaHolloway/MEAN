6264a243e598a4f10e3efbce5745947f99573630
JoshuaHolloway <jhollow6@asu.edu>
2020-02-13 16:11:12 -0600

    Angular material mat-form-field inside a mat-card

---
+++ 02_Angular/205/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+
+    private enteredValue: string = '';
+    private newPost: string = 'NO CONTENT';
+    private newPost2: string = 'NO CONTENT 2';
+
+    onAddPost(postInput: HTMLInputElement): void {
+
+        console.log(postInput);
+        console.dir(postInput);
+
+        alert('Post added!');
+
+        //this.newPost = 'The user\'s post';
+        this.newPost = postInput.value;
+
+        // [204]
+        this.newPost2 = this.enteredValue;
+    }
+}
